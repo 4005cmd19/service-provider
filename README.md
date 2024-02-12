e68f50db91a73b6594299fa22fc0e02ca3e01106
max <max@guavaapps.com>
2024-02-12 01:15:40 +0000

    add files from aws lambda

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# service-provider
+# Service provider for live bus information
+
+This is an AWS Lambda function scheduled to run periodically and listen to messages on specific MQTT topics and respond accordingly.
+
+It uses the [mqtt.js](https://github.com/mqttjs/MQTT.js) library to connect to our [HiveMQ]() broker.
+
+## 
--- index.mjs
+++ index.mjs
@@ -80,17 +80,3 @@ export const handler = async (event) => {
     };
     return response;
 };
-
-const testEvent = {
-    ttl: 10000,
-    debug: true,
-    mqtt_connect_timeout: 10000,
-    mqtt_host: "61af2c55ce064492b81b2877be486551.s2.eu.hivemq.cloud",
-    mqtt_port: 8883,
-    mqtt_app_id: "mqtt-4005-main",
-    mqtt_app_key: "MQTT-4005-connect",
-    mqtt_protocol: "ssl"
-}
-
-
-console.log(handler(testEvent).then(it => console.log(`it - ${JSON.stringify(it, null, 2)}`)))
